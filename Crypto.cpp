5efa8b716065c00657d761a2f71acee2b19d5f22
Michael lemito <delovoy.lemit@gmail.com>
2025-10-01 21:27:49 +0300

    meow

--- Crypto.cpp
+++ Crypto.cpp
@@ -8,16 +8,5 @@ using namespace std;
 
 int main() {
   meow::cypher::test_module();
-  auto [d, x, y] = meow::math::eGCD(240, 46);
-  const meow::math::primary::FermatTest f;
-  const meow::math::primary::MillerRabinTest mr;
-  std::cout << f.isPrimary(7, 0.9998) << std::endl;
-  // std::cout << mr.roundCnt(0.9998) << ' ' << mr.probabilisticCoeff <<std::endl;
-  // std::cout << d << " " << x << ' ' << y << std::endl;
-  // std::cout << "GCD(48, 18) = " << meow::math::GCD(48, 18) << std::endl;
-  // std::cout << "GCD(17, 13) = " << meow::math::GCD(17, 13) << std::endl;
-  // std::cout << "GCD(0, 5) = " << meow::math::GCD(0, 5) << std::endl;
-  // std::cout << meow::math::LejandreSymbol(12345, 331) << std::endl;
-  // std::cout << meow::math::JacobiSymbol(219, 383) << std::endl;
   return 0;
 }
---
+++ include/math.h
@@ -0,0 +1,10 @@
+//
+// Created by lemito on 10/1/25.
+//
+
+#ifndef CRYPTOMAI_MATH_H
+#define CRYPTOMAI_MATH_H
+
+import math;
+
+#endif  // CRYPTOMAI_MATH_H
