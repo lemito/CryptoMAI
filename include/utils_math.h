1a3a96b0a3c038d9790fdd3b6c03e8d3e37d8bbd
Michael lemito <delovoy.lemit@gmail.com>
2025-10-02 21:41:40 +0300

    reformat

--- include/utils_math.h
+++ include/utils_math.h
@@ -1,10 +1,7 @@
-#ifndef CRYPTOMAI_MATH_H
-#define CRYPTOMAI_MATH_H
+#pragma once
 
+#include <algorithm>
 #include <boost/multiprecision/gmp.hpp>
 #include <boost/random.hpp>
-#include <algorithm>
 
 using BI = boost::multiprecision::mpz_int;
-
-#endif  // CRYPTOMAI_MATH_H
