2dc2092190cb0b970e91e6d2265c23dab34aca84
Michael lemito <delovoy.lemit@gmail.com>
2025-10-04 00:13:41 +0300

    тесты рса

--- tests/test_rsa.cpp
+++ tests/test_rsa.cpp
@@ -1,3 +1,31 @@
 #include <gtest/gtest.h>
+
+#include "utils_math.h"
 import cypher.RSA;
 
+TEST(RSA, Simple) {
+  const auto some_msg = BI("74123657855656565836662666");
+  const meow::cypher::RSA::RSAService service{
+      meow::cypher::RSA::RSAService::KeyGen::PrimaryTests::MillerRabinTest,
+      0.95, 1024};
+  const auto res = service.encrypt(some_msg);
+  const auto res2 = service.decrypt(res);
+  ASSERT_TRUE(some_msg == res2);
+}
+
+int main(int argc, char** argv) {
+  // try {
+  //
+  // } catch (...) {
+  // }
+  // const auto some_msg = BI("74123657855656565836662666");
+  // const meow::cypher::RSA::RSAService service{
+  //     meow::cypher::RSA::RSAService::KeyGen::PrimaryTests::MillerRabinTest,
+  //     0.95, 1024};
+  // const auto res = service.encrypt(some_msg);
+  // const auto res2 = service.decrypt(res);
+  // std::cout << "Ожидалось " << some_msg << "  " << res2 << "\n";
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+  // return 0;
+}
