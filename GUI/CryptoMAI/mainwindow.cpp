a2b571323cc44ca3cd66c7e49096c40180a2ccde
Michael lemito <delovoy.lemit@gmail.com>
2025-11-14 00:58:39 +0300

    ///

--- GUI/CryptoMAI/mainwindow.cpp
+++ GUI/CryptoMAI/mainwindow.cpp
@@ -2,6 +2,9 @@
 
 #include "./ui_mainwindow.h"
 
-
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+}
 
 MainWindow::~MainWindow() { delete ui; }
