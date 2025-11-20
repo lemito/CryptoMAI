f2f6db82c00188acdb1589d1215d6ff7681a0449
Michael lemito <delovoy.lemit@gmail.com>
2025-11-20 15:14:03 +0300

    окей, больше тестов

--- tests/test_rc6.cpp
+++ tests/test_rc6.cpp
@@ -130,6 +130,46 @@ TEST(RC6, Simple) {
   ASSERT_EQ(BUFFER_res, plain);
 }
 
+TEST(RC6, ALL) {
+  const std::vector key{
+    std::byte{0x8e}, std::byte{0x73}, std::byte{0xb0}, std::byte{0xf7},
+    std::byte{0xda}, std::byte{0x0e}, std::byte{0x64}, std::byte{0x52},
+    std::byte{0xc8}, std::byte{0x10}, std::byte{0xf3}, std::byte{0x2b},
+    std::byte{0x80}, std::byte{0x90}, std::byte{0x79}, std::byte{0xe5},
+    std::byte{0x62}, std::byte{0xf8}, std::byte{0xea}, std::byte{0xd2},
+    std::byte{0x52}, std::byte{0x2c}, std::byte{0x6b}, std::byte{0x7b}};
+
+  const auto algo =
+      std::static_pointer_cast<meow::cypher::symm::ISymmetricCypher>(
+          std::make_shared<meow::cypher::symm::RC6::RC6>());
+
+  const std::vector IV{
+    static_cast<std::byte>(0x32), static_cast<std::byte>(0x43),
+    static_cast<std::byte>(0xf6), static_cast<std::byte>(0xa8),
+    static_cast<std::byte>(0x88), static_cast<std::byte>(0x5a),
+    static_cast<std::byte>(0x30), static_cast<std::byte>(0x8d),
+    static_cast<std::byte>(0x31), static_cast<std::byte>(0x31),
+    static_cast<std::byte>(0x98), static_cast<std::byte>(0xa2),
+    static_cast<std::byte>(0xe0), static_cast<std::byte>(0x37),
+    static_cast<std::byte>(0x07), static_cast<std::byte>(0x34)};
+
+  for (size_t mode = 0; mode < 7; ++mode) {
+    for (size_t pad = 0; pad < 4; ++pad) {
+      auto ctx = meow::cypher::symm::SymmetricCypherContext(
+          key, static_cast<meow::cypher::symm::encryptionMode>(mode),
+          static_cast<meow::cypher::symm::paddingMode>(pad), IV, BI(52));
+      ctx.setAlgo(algo);
+
+      std::string pref = std::to_string(mode) + "_" + std::to_string(pad);
+
+      ctx.encrypt(pref + "BUFFER", "2.txt");
+      ctx.decrypt(pref + "BUFFER_res_rc6", pref + "BUFFER");
+
+      ASSERT_TRUE(isFilesEqual("2.txt", pref + "BUFFER_res_rc6"));
+    }
+  }
+}
+
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
